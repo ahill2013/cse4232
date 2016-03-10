81ec7f108ad1aa596b90e67b10a819df5a828d87
wnyffenegger2013@my.fit.edu <wnyffenegger2013@my.fit.edu>
2016-03-10 18:29:07 -0500

    UDP Threading done

--- src/UDPHandler.java
+++ src/UDPHandler.java
@@ -23,31 +23,61 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.sql.SQLException;
 
 public class UDPHandler implements Runnable {
 
-    DatagramSocket sock;
-    DatagramPacket packet;
+    private int _port;
+    private boolean _running = true;
+    private LogicEngine engine;
 
-    public UDPHandler(int port) throws SocketException {
-        sock = new DatagramSocket(port);
+    public UDPHandler(int port, String dbFile) throws SocketException {
+        _port = port;
+        engine = new LogicEngine(dbFile);
     }
 
     @Override
     public void run() {
+        try {
+            byte[] buffer = new byte[65508];
+            DatagramSocket socket = new DatagramSocket(_port);
+            DatagramPacket receive = new DatagramPacket(buffer, buffer.length);
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    engine.closeLogicEngine();
+                }
+            });
 
-        byte[] buffer = new byte[65508];
-        packet = new DatagramPacket(buffer, buffer.length);
+            while (_running) {
+                try {
 
-        for (;;) {
-            try {
-                sock.receive(packet);
-                //doStuffWithData(packet, buffer)?
-                System.out.println(buffer.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
+                    socket.receive(receive);
+                    InetAddress packet_address = receive.getAddress();
+                    int packet_port = receive.getPort();
+
+                    byte[] reply = engine.parseInput(new String(receive.getData()), packet_address.toString(), packet_port).getBytes();
+
+                    DatagramPacket send = new DatagramPacket(reply, reply.length, receive.getAddress(), receive.getPort());
+                    socket.send(send);
+
+                    System.out.println(buffer.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    System.out.println("Could not open database for UDP packet");
+                }
             }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } finally {
+            engine.closeLogicEngine();
         }
+    }
+
+    public void stop() {
+        _running = false;
     }
 }

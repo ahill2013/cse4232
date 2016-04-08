f1da33d1277379eb8f4c2d784808f9ef311a989b
ahill2013 <ahill2013@my.fit.edu>
2016-04-08 17:59:44 -0400

    little progress made on EncoderDecoder.

--- src/client/EncoderDecoder.java
+++ src/client/EncoderDecoder.java
@@ -1,5 +1,6 @@
 package client;
 
+import asn1objects.ASN1GetProjects;
 import net.ddp2p.ASN1.Decoder;
 import net.ddp2p.ASN1.Encoder;
 
@@ -13,4 +14,13 @@ public class EncoderDecoder {
         return new Decoder(response).toString();
     }
 
+    public static byte[] encodeProjects(String command) {
+        ASN1GetProjects getProjects = new ASN1GetProjects();
+        Encoder enc = getProjects.getEncoder();
+        return enc.getBytes();
+    }
+
+    public static String decodeProjects(byte[] response) {
+
+    }
 }

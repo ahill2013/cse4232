db86be80a7b9e1ebfb24ef5900508dc8a188f990
wnyffenegger2013@my.fit.edu <wnyffenegger2013@my.fit.edu>
2016-04-30 13:09:48 -0400

    enterprise

--- src/client/Client.java
+++ src/client/Client.java
@@ -120,6 +120,7 @@ public class Client {
                 try {
                     serverInput = ClientParser.parseClientInput(input, new SimpleDateFormat("yyyy-MM-dd:hh'h'mm'm'ss's'SSS'Z'"));
 
+
                     if (cmd.hasOption("d")) {
                         if (cmd.hasOption("u")) sendCommandUDP(sock, serverInput, cmd.getOptionValue("d"), port);
                         else sendCommandTCP(serverInput, cmd.getOptionValue("d"), port);
